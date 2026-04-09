d031d524c64fc99c874dd3a6ecf7154705991b44
Williams Onuaguluchi <williamsonuaguluchi4991@gmail.com>
2026-04-09 18:22:08 +0100

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,7 @@ else
     # Install standalone docker-compose
     sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/deploy.sh
     DOCKER_COMPOSE="docker-compose"
 fi
 
